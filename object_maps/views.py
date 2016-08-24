f9d41cf1aabeb2b7a59dc3bba0a353d2713fe749
smin <smin@daegu.ac.kr>
2016-08-24 13:54:57 +0900

    Changed the views for the site

--- object_maps/views.py
+++ object_maps/views.py
@@ -9,3 +9,16 @@ def hello(request):
 def Insertobject(request, id, objectX, objectY, width, height):
 	objects(id=id, objectX=objectX, objectY=objectY, width=width, height=height).save()
 	#return render(request, 'object_maps/mypage.html',{'welcome_text': 'Insert: ' + id})
+	return HttpResponse("Success! id: %s" %id)
+	
+def viewobject():
+	for row in objects.objects.all():
+		id = str(row.id)
+		objectX = str(row.objectX)
+		objectY = str(row.objectY)
+		width = str(row.width)
+		height = str(row.height)
+		
+		data = "id: " + id + ", objectX: " + objectX + ", objectY: " + objectY + ", width: " + width + ", height: " + height
+		return HttpResponse(data)
+	return HttpResponse("end")
